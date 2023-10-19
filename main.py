8100c72305feb7319ebaf13d2a6f3c1289849897
Victor Anyadioha <victoranyadioha@gmail.com>
2023-10-19 17:47:23 +0100

    Feat: Change

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ screen = Screen()
 screen.title("U.S States Quiz")
 screen.addshape("usmap.gif")
 shape("usmap.gif")
-states_left = True
 
 states = pd.read_csv("50states.csv")
 states_list = states.state.to_list()
-while states_left:
-    states_left = len(states_list)
+guessed = []
+while len(guessed) < 50:
     answer = screen.textinput(
-        title=f"{50-states_left}/50 States Left",
+        title=f"{len(guessed)}/50 States Left",
         prompt="Enter the name of a state in the US",
     ).title()
     if answer in states_list:
@@ -23,8 +22,13 @@ while states_left:
         x = int(answer_row.x)
         y = int(answer_row.y)
         state_name.goto(x, y)
-        state_name.write(f"{answer}", align="center", font=("Courier", 10, "normal"))
-        states_list.remove(answer)
-    else:
-        pass
-mainloop()
+        state_name.write(answer, align="center", font=("Courier", 6, "normal"))
+        guessed.append(answer)
+    elif answer == "Exit":
+        unguessed = []
+        for state in states_list:
+            if state not in guessed:
+                unguessed.append(state)
+        pd_unguessed = pd.DataFrame(unguessed)
+        pd_unguessed.to_csv("./results.csv")
+        break
